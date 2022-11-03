0d2143c8e21f8178edb109380134df01a5041057
AliAkbarov99 <tu6lw8g66@code.edu.az>
2022-11-03 12:14:20 +0400

    array task updated

---
+++ index.js
@@ -0,0 +1,34 @@
+//  let arr = [1,2,3,4];
+
+// let s;
+// for ( s = 0; s < arr.length; s++){ //n
+//     console.log(arr[s]) //n
+// }
+
+// for (let s = 0; s < arr.length; s++) //n
+// {
+//     console.log(arr[s] + 1); //n
+// }
+
+
+function Sum(a,b){
+    return a + b;
+}
+
+function multiply(a,b){
+    return a * b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function divide(a,b){
+    return a / b;
+}
+
+
+let c = Sum(4,5)
+console.log(c);
+
+
